4e10b4e65b733d6eb52eff7122e365bc9bcc44e2
Sinatrio Bimo Wahyudi <44100152+sinatriiobimo@users.noreply.github.com>
2020-10-27 13:31:43 +0700

    Complete Code

---
+++ js/script.js
@@ -0,0 +1,43 @@
+console.log("It's working")
+
+let theme = localStorage.getItem('theme')
+if(theme == null) {
+    setTheme('light')
+} else {
+    setTheme(theme)
+}
+
+let themeDots = document.getElementsByClassName('theme-dot')
+
+for(var i=0; themeDots.length > i; i++) {
+    themeDots[i].addEventListener('click', function() {
+        let mode = this.dataset.mode
+        console.log('Option Click!', mode)
+        setTheme(mode)
+    })
+}
+
+function setTheme(mode) {
+    if(mode == 'light') {
+        document.getElementById('theme-style').href = '/css/style.css'
+    }
+    if(mode == 'blue') {
+        document.getElementById('theme-style').href = '/css/blue.css'
+    }
+    if(mode == 'green') {
+        document.getElementById('theme-style').href = '/css/green.css'
+    }
+    if(mode == 'purple') {
+        document.getElementById('theme-style').href = '/css/purple.css'
+    }
+    localStorage.setItem('theme', mode)
+}
+
+// FADEIN
+const splash = document.querySelector('.splash')
+
+document.addEventListener('DOMContentLoaded', (e) => {
+    setTimeout(() => {
+        splash.classList.add('display-none')
+    }, 2000)
+})
